a1fe7f39812d589f63e77dccb29b95a7eb19a139
duddk <duddk@scatterx.io>
2025-04-21 15:53:55 +0900

    중첩 객체 상태 관리

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import AppXY from './AppXY.jsx'
+import AppMentor from './AppMentor.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <AppXY />
+    {/* <AppXY /> */}
+    <AppMentor />
   </StrictMode>,
 )
