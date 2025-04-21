7ce2b32d4f398f3e6fe2675a1c2740435199241c
duddk <duddk@scatterx.io>
2025-04-21 15:30:08 +0900

    마우스 따라가기 - 기본구현

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import AppXY from './AppXY.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <AppXY />
   </StrictMode>,
 )
