69e90b0092305f3dd8114c820916abec9b2c9f26
duddk <duddk@scatterx.io>
2025-04-22 10:41:00 +0900

    form을 만드는법

--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,13 @@ import './index.css'
 import AppXY from './AppXY.jsx'
 import AppMentor from './AppMentor.jsx'
 import AppMentors from './AppMentors.jsx'
+import AppForm from './AppForm.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     {/* <AppXY /> */}
     {/* <AppMentor /> */}
-    <AppMentors />
+    {/* <AppMentors /> */}
+    <AppForm />
   </StrictMode>,
 )
